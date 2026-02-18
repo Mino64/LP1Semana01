f351957f0ca0e2ad0374e90cd728e97d04c8d0eb
Daniel <daniel.henriques.gamedev@gmail.com>
2026-02-18 15:47:09 +0000

    added comments

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Program is the Program that starts the method Main
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Main writes Hello LP and Bye World in the console
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP!");
