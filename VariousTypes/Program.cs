dfba46a43daff2e0e822cc673d41c6307cd108ef
Daniel <daniel.henriques.gamedev@gmail.com>
2026-02-18 16:16:19 +0000

    added real numbers and booleans

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -7,16 +7,24 @@ namespace VariousTypes
         private static void Main(string[] args)
         {
             int firstNumber = 5;
-            bool falseCondition = false;
+            short secondNumber = 7;
             string name = "Ken";
             char heartEmoji = '\u2661';
             char VoltageEmoji = '\u26A1';
+            float realFloat = 45.5f;
+            double realDouble = 33.9d;
+            bool falseCondition = false;
+            bool trueCondition = true;
 
             Console.WriteLine(firstNumber);
-            Console.WriteLine(falseCondition);
+            Console.WriteLine(secondNumber);
             Console.WriteLine(name);
             Console.WriteLine(heartEmoji);
             Console.WriteLine(VoltageEmoji);
+            Console.WriteLine(realFloat);
+            Console.WriteLine(realDouble);
+            Console.WriteLine(falseCondition);
+            Console.WriteLine(trueCondition);
         }
     }
 }
