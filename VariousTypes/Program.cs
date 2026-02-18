bd16244b767bbcd6eeedbc7c31dbf2b78a4cbd76
Daniel <daniel.henriques.gamedev@gmail.com>
2026-02-18 16:01:12 +0000

    renamed LPSemana01 to LP1Semana01 + VariousTypes added

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int firstNumber = 5;
+            bool falseCondition = false;
+            string name = "Ken";
+
+            Console.WriteLine(firstNumber);
+            Console.WriteLine(falseCondition);
+            Console.WriteLine(name);
+        }
+    }
+}
