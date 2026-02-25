5e434f0d20c89837069c9c6b882882af80313e27
Daniel <daniel.henriques.gamedev@gmail.com>
2026-02-25 21:47:09 +0000

    exercice 6 finished

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -7,14 +7,14 @@ namespace ManyStrings
         private static void Main(string[] args)
         {
             // number with decimal
+            double xx = 1.23456;
+            int ii = 19;
             double variable = 1.234;
             float x = MathF.PI;
             string testingEmoji ="this be an emoji \u2622";
             string unicode = "this is a \t tab ";
             string concat = "a"+2;
             string concatPi = "the number of Pi is " + x;
-
-
 
             // f1 = .0 (aka it only goes one behind decimal)
             string interpolation1 = $"x={variable:f1}";
@@ -28,6 +28,8 @@ namespace ManyStrings
             /// Percentage is also only between 0 - 1
             string interpolation3 =$"'{5, -6:f2}' e '{0.2:p1}'";
 
+            string realxx = $"xx in decimals= {xx:f2} and percentage= {xx:p1}";
+            string hexaii = $"ii in hexa= {ii:x} and ii in moeda= {ii:c}";
         
             Console.WriteLine(testingEmoji);
             Console.WriteLine(unicode);
@@ -36,6 +38,8 @@ namespace ManyStrings
             Console.WriteLine(interpolation1);
             Console.WriteLine(interpolation2);
             Console.WriteLine(interpolation3);
+            Console.WriteLine(realxx);
+            Console.WriteLine(hexaii);
 
         }
     }
