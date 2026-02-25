b1b6b3e3b0bdd343e052c7860b4548b24e1b9917
Daniel <daniel.henriques.gamedev@gmail.com>
2026-02-25 21:34:31 +0000

    added interpolation and concatenation

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,11 +6,36 @@ namespace ManyStrings
     {
         private static void Main(string[] args)
         {
+            // number with decimal
+            double variable = 1.234;
+            float x = MathF.PI;
             string testingEmoji ="this be an emoji \u2622";
             string unicode = "this is a \t tab ";
+            string concat = "a"+2;
+            string concatPi = "the number of Pi is " + x;
+
+
+
+            // f1 = .0 (aka it only goes one behind decimal)
+            string interpolation1 = $"x={variable:f1}";
+
+            ///x = number in hex (10 will become a)
+            /// 4 and -4 are alignment, 4 will be "4 spaces" before 10 (in hex)
+            string interpolation2 = $".{10,4:x}. e .{11,-4:x}";
+
+            ///f2 = .00 (2 behind the decimal)
+            /// p1 = .0 % (percentage with one number in decimal)
+            /// Percentage is also only between 0 - 1
+            string interpolation3 =$"'{5, -6:f2}' e '{0.2:p1}'";
+
         
             Console.WriteLine(testingEmoji);
             Console.WriteLine(unicode);
+            Console.WriteLine(concat);
+            Console.WriteLine(concatPi);
+            Console.WriteLine(interpolation1);
+            Console.WriteLine(interpolation2);
+            Console.WriteLine(interpolation3);
 
         }
     }
