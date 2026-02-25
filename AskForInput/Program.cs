feac7f93646e8f93c27b6d643152f89c91436e6c
Daniel <daniel.henriques.gamedev@gmail.com>
2026-02-25 22:01:40 +0000

    exercice 7 finished

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -6,7 +6,16 @@ namespace AskForInput
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            Console.WriteLine("give me an int number");
+            string str = Console.ReadLine();
+            int i = Convert.ToInt32(str);
+
+            Console.WriteLine("give me a float number");
+            string str2 = Console.ReadLine();
+            float f= Convert.ToSingle(str2);
+
+            Console.WriteLine($"{i} + {f} = {i + f}");
+
         }
     }
 }
